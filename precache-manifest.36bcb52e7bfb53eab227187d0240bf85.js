58f820f77aa84e99404c972e42d630b57965ff39
SonLV <apple@SonLV.local>
2019-09-14 17:36:56 +0700

    Updates

---
+++ precache-manifest.36bcb52e7bfb53eab227187d0240bf85.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b224c9884c2bb081d4ac92b23675c2e",
+    "url": "/favico.png"
+  },
+  {
+    "revision": "adc7aa6d546225fed64452b4081c57ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e957b5d9ecf70f7e3131",
+    "url": "/index_bundle.js"
+  }
+]);
---
+++ precache-manifest.87d510020ee71f04b4fca2de3ea7f2ec.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b224c9884c2bb081d4ac92b23675c2e",
+    "url": "favico.png"
+  },
+  {
+    "revision": "1c26af5c200fb6cec8034dea28144e29",
+    "url": "index.html"
+  },
+  {
+    "revision": "cd55a097b45db7cffdee",
+    "url": "main.js"
+  }
+]);
