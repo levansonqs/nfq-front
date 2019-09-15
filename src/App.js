b5862cc4ac15471e56695ef5530cc140c2555e6c
SonLV <apple@SonLV.local>
2019-09-15 11:21:38 +0700

    add deploy github page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: sonlv@nexlesoft.com
  * @Date: 2019-09-11 21:49:03
  * @Last Modified by: sonlv@nexlesoft.com
- * @Last Modified time: 2019-09-14 11:41:03
+ * @Last Modified time: 2019-09-14 17:57:28
  */
 import React, { memo } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -12,7 +12,7 @@ import Video from './pages/Video';
 
 export default memo(function App() {
   return (
-    <Router>
+    <Router basename="nfq-front/">
       <div>
         <Route exact path="/" component={Home} />
         <Route path="/nasa-search" component={NasaSearch} />
---
+++ webpack.production.config.js
@@ -0,0 +1,79 @@
+/*
+ * @Author: sonlv@nexlesoft.com
+ * @Date: 2019-09-14 17:18:49
+ * @Last Modified by: sonlv@nexlesoft.com
+ * @Last Modified time: 2019-09-14 17:47:43
+ */
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/nfq-front'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: true // true outputs JSX tags
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      title: 'Progressive Web Application',
+      favicon: './favico.png'
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true
+    })
+  ]
+};
